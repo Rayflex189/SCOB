34f9a20e8eb1560c07aa070378f3108cf8777518
Coder Black <165771215+Rayflex189@users.noreply.github.com>
2025-04-04 08:20:17 +0100

    Update wsgi.py
    Save

--- SCOB/wsgi.py
+++ SCOB/wsgi.py
@@ -8,11 +8,28 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+from django.contrib.auth import get_user_model
+from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SCOB.settings')
 
 application = get_wsgi_application()
+app = application  # Alias for compatibility with some hosting services
 
-app = application
+def create_superuser():
+    """
+    Ensures a permanent superuser is created at server startup.
+    """
+    User = get_user_model()
+    username = settings.SUPERUSER_USERNAME
+    email = settings.SUPERUSER_EMAIL
+    password = settings.SUPERUSER_PASSWORD
+
+    if not User.objects.filter(username=username).exists():
+        User.objects.create_superuser(username=username, email=email, password=password)
+        print(f"Superuser '{username}' created successfully.")
+    else:
+        print(f"Superuser '{username}' already exists.")
+
+create_superuser()
