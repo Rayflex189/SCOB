3e0e1acb41e442f63fd139ec0e18d04701407fab
Coder Black <165771215+Rayflex189@users.noreply.github.com>
2025-04-05 18:32:21 +0100

    Update settings.py
    Save

--- SCOB/settings.py
+++ SCOB/settings.py
@@ -63,7 +63,7 @@ INSTALLED_APPS = [
 
 
 CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': 'heritage_dbs',
+    'CLOUD_NAME': 'dlzn0moho',
     'API_KEY': '942887989436842',
     'API_SECRET': '8_Hu2A6oefhgbHWGdA0cEehYerc',
 }
