f42d91fd457b30eb113ecc3566dfdbdb6179f085
Coder Black <165771215+Rayflex189@users.noreply.github.com>
2025-04-05 17:04:50 +0100

    Update settings.py
    Save

--- SCOB/settings.py
+++ SCOB/settings.py
@@ -56,7 +56,19 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.humanize',
     'bank_app',
+    'cloudinary',
+    'cloudinary_storage',
 ]
+
+
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': 'your_cloud_name',
+    'API_KEY': 'your_api_key',
+    'API_SECRET': 'your_api_secret',
+}
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
