2475d234569eb185fba7d34c4545a231997ee067
Coder Black <165771215+Rayflex189@users.noreply.github.com>
2025-04-05 23:57:24 +0100

    Update settings.py
    Save

--- SCOB/settings.py
+++ SCOB/settings.py
@@ -50,7 +50,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1fw)(+*&**t4_r@=-a%9)hstji&^ueabvfx)4=csjoafvzhnwg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False 
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
