f8db74f269db160cc1625084d48a8fd774073516
Coder Black <165771215+Rayflex189@users.noreply.github.com>
2025-04-06 00:50:12 +0100

    Update build.sh
    Save

--- build.sh
+++ build.sh
@@ -1,19 +1,18 @@
 #!/usr/bin/env bash
-# Exit on any error
+# Exit on error
 set -o errexit
 
 # Install dependencies
 pip install -r requirements.txt
 
+# Remove previous migration files except __init__.py
+find bank_app/migrations/ -type f ! -name '__init__.py' -delete
+
+# Make fresh migrations
+python manage.py makemigrations
+
+# Apply migrations with fake initial if tables already exist
+python manage.py migrate --fake-initial
+
 # Collect static files
 python manage.py collectstatic --no-input
-
-# Recreate migration files
-find bank_app/migrations/ -type f -not -name '__init__.py' -delete
-
-python manage.py makemigrations bank_app
-
-# Apply them with --fake to tell Django the schema already exists
-python manage.py migrate bank_app --fake
-# Optional: Start the server (uncomment and set your project name)
-# gunicorn project_name.wsgi:application
