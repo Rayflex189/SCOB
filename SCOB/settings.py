003c0c7fbe7874963eb0d043ccc1a5e559a952fa
Coder Black <165771215+Rayflex189@users.noreply.github.com>
2025-05-16 11:07:10 +0100

    Update settings.py
    save

--- SCOB/settings.py
+++ SCOB/settings.py
@@ -29,7 +29,9 @@ cloudinary.config(
 
 DATABASES = {
     'default': dj_database_url.parse(
-        '//postgres:[8K6brYhGTjBg59TY]@db.plwjvpsceexklfuxcuqh.supabase.co:5432/postgres'
+        'postgres://postgres:8K6brYhGTjBg59TY@db.plwjvpsceexklfuxcuqh.supabase.co:5432/postgres',
+        conn_max_age=600,
+        ssl_require=True
     )
 }
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
