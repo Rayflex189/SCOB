b4e5a487fd82f42df2eb9df41e5a771ba653043b
Coder Black <165771215+Rayflex189@users.noreply.github.com>
2025-05-17 00:48:01 +0100

    Update settings.py
    Save

--- SCOB/settings.py
+++ SCOB/settings.py
@@ -28,7 +28,7 @@ cloudinary.config(
 
 DATABASES = {
     'default': dj_database_url.parse(
-        'postgresql://postgres.plwjvpsceexklfuxcuqh:[YOUR-PASSWORD]@aws-0-us-west-1.pooler.supabase.com:6543/postgres',
+        'postgresql://postgres.plwjvpsceexklfuxcuqh:8K6brYhGTjBg59TY@db.plwjvpsceexklfuxcuqh.supabase.co@aws-0-us-west-1.pooler.supabase.com:6543/postgres',
         conn_max_age=600,
         ssl_require=True
     )
