9e475586ff9ab4b66fe4c55bcc081ceab83e536d
Coder Black <165771215+Rayflex189@users.noreply.github.com>
2025-05-16 11:28:40 +0100

    Update settings.py
    Save

--- SCOB/settings.py
+++ SCOB/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
+print("DATABASE_URL =", config("DATABASE_URL"))
 SUPERUSER_USERNAME = "admin"
 SUPERUSER_EMAIL = "admin@example.com"
 SUPERUSER_PASSWORD = "securepassword123"
