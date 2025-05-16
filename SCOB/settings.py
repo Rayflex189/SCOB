4ddfc873e22ed8fc1c3a069bb03ec879d31ecb4a
Coder Black <165771215+Rayflex189@users.noreply.github.com>
2025-05-16 09:28:26 +0100

    Update settings.py
    Save

--- SCOB/settings.py
+++ SCOB/settings.py
@@ -34,6 +34,8 @@ DATABASES = {
     )
 }
 
+print("DATABASE_URL:", config('DATABASE_URL'))
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
