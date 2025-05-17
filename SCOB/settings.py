b7ba66545c5a46b5c378ce15ce6195f15a40dc6b
Coder Black <165771215+Rayflex189@users.noreply.github.com>
2025-05-17 15:54:42 +0100

    Update settings.py
    Save

--- SCOB/settings.py
+++ SCOB/settings.py
@@ -28,14 +28,11 @@ cloudinary.config(
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres.plwjvpsceexklfuxcuqh',
-        'PASSWORD': '8K6brYhGTjBg59TY',
-        'HOST': 'aws-0-us-west-1.pooler.supabase.com',
-        'PORT': '6543',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
