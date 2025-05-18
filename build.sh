7f2f0ef3e2ee6c9bd7fe9190e50e2d10de803a21
Coder Black <165771215+Rayflex189@users.noreply.github.com>
2025-05-18 20:03:08 +0100

    Update build.sh
    Save

--- build.sh
+++ build.sh
@@ -1,1 +1,14 @@
+#!/usr/bin/env bash
 
+# Exit immediately if a command exits with a non-zero status
+set -o errexit
+
+# Install Python dependencies
+pip install -r requirements.txt
+
+# Run database migrations
+python manage.py makemigrations
+python manage.py migrate --noinput
+
+# Collect static files for production
+python manage.py collectstatic --no-input
