9af5bcf8ba2ee7f2d9e24e29c8b768df0d4f6b46
Coder Black <165771215+Rayflex189@users.noreply.github.com>
2025-05-18 22:40:50 +0100

    Update build.sh
    Save

--- build.sh
+++ build.sh
@@ -1,15 +1,14 @@
-#!/usr/bin/env bash
-# Exit on error
-set -o errexit
+#!/bin/bash
 
-# Install dependencies
+# Upgrade pip
+pip install --upgrade pip
+
+# Install dependencies (if using poetry or requirements.txt)
+# If using poetry:
+# pip install poetry && poetry config virtualenvs.create false && poetry install
+
+# If using requirements.txt
 pip install -r requirements.txt
 
-# Make fresh migrations
-python manage.py makemigrations
-
-# Apply migrations with fake initial if tables already exist
-python manage.py migrate --fake-initial
-
-# Collect static files
-python manage.py collectstatic --no-input
+# Collect static files (optional for production)
+python manage.py collectstatic --noinput
